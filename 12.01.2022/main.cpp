0b0f177307c1509caaef4d84d709a6a2453404e0
Przybylskii <90185127+Przybylskii@users.noreply.github.com>
2022-01-12 15:40:28 +0100

    Add files via upload

---
+++ 12.01.2022/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+
+class bird{
+	public: 
+	int pub = 0;
+	private:
+	int pri =0;
+	protected:
+	int pro =0;
+};
+
+class parrot :  bird{
+	
+};
+class kiwi :  bird{
+	
+};
+	
+
+
+
+
+
+
+
+int main(int argc, char** argv) {
+	setlocale(LC_CTYPE,"polish");
+	
+	
+	parrot moment;
+	
+	
+	
+	
+	return 0;
+}
